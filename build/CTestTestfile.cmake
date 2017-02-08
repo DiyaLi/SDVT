19028037d2a3c3e3bc3743abf04e59069f415c69
diya <lydia1@vt.edu>
2017-02-08 15:09:48 -0500

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/diya/sdvt2017/src
+# Build directory: /home/diya/sdvt2017/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(sdvt_description)
+SUBDIRS(sdvt_gazebo)
+SUBDIRS(sdvt_viz)
---
+++ build/sdvt_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diya/sdvt2017/src/sdvt_gazebo
+# Build directory: /home/diya/sdvt2017/build/sdvt_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
